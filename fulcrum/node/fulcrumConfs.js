ed0811f30bc21c57b7c7d048c63160babe448063
Kenn Herman <kenn@ifsight.com>
2016-11-15 13:44:34 -0800

    util should output content, not alter files, unify all mapping

--- fulcrum/node/fulcrumConfs.js
+++ fulcrum/node/fulcrumConfs.js
@@ -1,8 +1,11 @@
 // read each json file in conf directory
 var fs      = require('fs'),
     cfg_dir = process.argv[2],
-    content = '',
-    cfgs;
+    cnfMap = '',
+    envMap = '',
+    webMap = '',
+    cfgs,
+    site;
 
 if (cfg_dir !== undefined) {
   cfgs    = fs.readdirSync(cfg_dir);
@@ -10,11 +13,16 @@ if (cfg_dir !== undefined) {
   for (var i = 0; i < cfgs.length; i++) {
     if (cfgs[i].match(/\.json$/)) {
       conf = require(cfg_dir + '/' + cfgs[i]);
-      content += '  ' + cfgs[i].replace(/\.json$/, '') + '    \'' + JSON.stringify(conf)  + '\';\n';
+      site = cfgs[i].replace(/\.json$/, '');
+      envMap += '  ' + site + '    \'' + conf.env             + '\';\n';
+      webMap += '  ' + site + '    \'' + conf.webroot         + '\';\n';
+      cnfMap += '  ' + site + '    \'' + JSON.stringify(conf) + '\';\n';
     }
   }
 
-  process.stdout.write('map $host $fulcrum_conf {\n  hostnames;\n\n' + content + '}\n');
+  process.stdout.write('map $host $fulcrum_env     {\n  hostnames;\n\n' + envMap + '}\n\n');
+  process.stdout.write('map $host $fulcrum_webroot {\n  hostnames;\n\n' + webMap + '}\n\n');
+  process.stdout.write('map $host $fulcrum_conf    {\n  hostnames;\n\n' + cnfMap + '}\n');
 } else {
   process.stdout.write('USAGE: ' + process.argv[1] + ' <CONFDIR>\n');
 }
