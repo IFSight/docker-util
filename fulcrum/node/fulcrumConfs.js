093e6eb09c177e48601df4e6d8599694f812dd9e
Kenn Herman <kenn@ifsight.com>
2016-12-08 12:39:59 -0800

    added complexity to deal with aliases

--- fulcrum/node/fulcrumConfs.js
+++ fulcrum/node/fulcrumConfs.js
@@ -1,29 +1,47 @@
+/**
+ * @file
+ * This node app works to parse fulcrum site configs.
+ */
+
 var input = '';
 
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', function(chunk) {
+process.stdin.on('data', function (chunk) {
   input += chunk;
 });
 
-process.stdin.on('end', function() {
+process.stdin.on('end', function () {
   var cfgs    = JSON.parse(input),
       envMap  = '',
       webMap  = '',
       cnfMap  = '',
+      sites   = {},
       site;
 
-  for (var i = 0; i < cfgs.length; i++) {
-    site = cfgs[i].webroot;
-
-    if (typeof cfgs[i].site !== 'undefined') {
-      site = cfgs[i].site;
+  /* make easier to address */
+  for (var i = 0; i < cfgs.sites.length; i++) {
+    /* if site is missing use webroot */
+    if (typeof cfgs.sites[i].site === 'undefined') {
+      cfgs.sites[i].site = cfgs.sites[i].site.webroot;
     }
 
-    envMap += '  ' + site + '    \'' + cfgs[i].env             + '\';\n';
-    webMap += '  ' + site + '    \'' + cfgs[i].webroot         + '\';\n';
-    cnfMap += '  ' + site + '    \'' + JSON.stringify(cfgs[i]) + '\';\n';
+    sites[cfgs.sites[i].site] = cfgs.sites[i];
+  }
+
+  /* add the aliases */
+  for (var src in cfgs.aliases) {
+    if (sites.hasOwnProperty(src)) {
+      sites[cfgs.aliases[src]] = sites[src];
+      sites[cfgs.aliases[src]].site = cfgs.aliases[src];
+    }
+  }
+
+  for (site in sites) {
+    envMap += '  ' + site + '    \'' + sites[site].env             + '\';\n';
+    webMap += '  ' + site + '    \'' + sites[site].webroot         + '\';\n';
+    cnfMap += '  ' + site + '    \'' + JSON.stringify(sites[site]) + '\';\n';
   }
 
   process.stdout.write('map $host $fulcrum_env     {\n  hostnames;\n\n' + envMap + '}\n\n');
